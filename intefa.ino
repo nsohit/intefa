6410a747266b919b1cd22bb80d3f684d8d386c07
nsohit <52670389+nsohit@users.noreply.github.com>
2019-12-24 14:41:12 +0700

    Add files via upload

---
+++ intefa.ino
@@ -0,0 +1,92 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  This example runs directly on ESP8266 chip.
+
+  Note: This requires ESP8266 support package:
+    https://github.com/esp8266/Arduino
+
+  Please be sure to select the right ESP8266 module
+  in the Tools -> Board menu!
+
+  Change WiFi ssid, pass, and Blynk auth token to run :)
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#include "DHT.h"
+
+#define DHTPIN 2   // what pin we're connected to
+#define relay 0
+
+
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "20b64f9214df444ca5362f254e38797e";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "sohit";
+char pass[] = "cobabuka";
+DHT dht(DHTPIN, DHTTYPE);
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+  dht.begin();
+  Blynk.begin(auth, ssid, pass);
+  pinMode(relay,OUTPUT);
+}
+
+void loop()
+{
+  Blynk.run();
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+    // Compute heat index in Fahrenheit (the default)
+  //float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+  Serial.println(hic);
+  delay(3000);
+  Blynk.virtualWrite(V1,hic);
+ if(hic >= 50){
+  digitalWrite(relay,LOW); 
+ }
+}
+
+ 
